209edd5291a348fab64108b6dad522750d1ea150
Abdelrahman Elnagdy <Abdelrahman.elnagdy.eng@gmail.com>
2020-12-16 21:00:14 +0200

    Revert "update server"
    This reverts commit 987af4646ec78ace7c4d4ac36fec4d84f99fe99e.

--- GameServer/src/gameserver/GameHandler.java
+++ GameServer/src/gameserver/GameHandler.java
@@ -10,9 +10,7 @@ package gameserver;
  * @author abdelrahmanelnagdy
  */
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.sql.SQLException;
@@ -26,8 +24,8 @@ import java.util.logging.Logger;
  */
 public class GameHandler extends Thread {
 
-    DataInputStream dataInputStream;
-    DataOutputStream dataOutputStream;
+    DataInputStream dis;
+    PrintStream ps;
     String[] parsedMsg;
     private DbConnection.DbConnectionHandler dbconnection;
 
@@ -35,10 +33,9 @@ public class GameHandler extends Thread {
 
     public GameHandler(Socket cs) {
         try {
-               dbconnection = DbConnection.DbConnectionHandler.CreateConnection();
-                dataOutputStream = new DataOutputStream(cs.getOutputStream());
-                dataInputStream = new DataInputStream(cs.getInputStream());
-                
+            dbconnection = DbConnection.DbConnectionHandler.CreateConnection();
+            dis = new DataInputStream(cs.getInputStream());
+            ps = new PrintStream(cs.getOutputStream());
             clientsVector.add(this);
             start();
         } catch (IOException ex) {
@@ -51,38 +48,30 @@ public class GameHandler extends Thread {
         while (true) {
 
             try {
-                String message = dataInputStream.readUTF();
-                System.out.println("The message sent from the socket was: " + message);
+                String message = dis.readLine();
 //                sendMessageToAll(str);
-                if (message == null) ; 
-                else if (parseMessage(message) == 1) {
+                if (message == null) ; else if (parseMessage(message) == 1) {
                     if (!checkUserExistence(parsedMsg[0])) {
                         addUser(parsedMsg[0], parsedMsg[1]);
                         ++MainServer.offlinePlayers;
                         updatePlayeStatus(parsedMsg[0]);
-                        System.out.print("Registered");
-                        dataOutputStream.writeUTF("register done");
+                        ps.println("register done");
                     } else {
                         System.out.println("User name is alreasdy in use");
-                        dataOutputStream.writeUTF("Cannot register player");
+                        ps.println("Cannot register player");
                     }
                 } else if (parseMessage(message) == 2) {
                     if (checkUserExistence(parsedMsg[0])) {
                         updatePlayeStatus(parsedMsg[0]);
                         ++MainServer.onlinePlayers;
                         --MainServer.offlinePlayers;
-                        dataOutputStream.writeUTF("sign in Succeeded#" + getPlayerScore(parsedMsg[0]));
+                        ps.println("sign in Succeeded#" + getPlayerScore(parsedMsg[0]));
                     } else {
-                        dataOutputStream.writeUTF("Cannot sign in");
+                        ps.println("Cannot sign in");
                     }
                 } else if (parseMessage(message) == 5) {
-                    dataOutputStream.writeUTF(dbconnection.getOnlinePlayersList());
+                    ps.println(dbconnection.getOnlinePlayersList());
                 }
-                
-                /*
-                dataOutputStream.flush();    // send the message
-                dataOutputStream.close();  */  // close the stream
-                
 
             } catch (IOException ex) {
                 stop();
@@ -94,10 +83,10 @@ public class GameHandler extends Thread {
 
     }
 
-    public void sendMessageToAll(String msg) throws IOException {
+    public void sendMessageToAll(String msg) {
         for (GameHandler sh : clientsVector) {
 
-            sh.dataOutputStream.writeUTF(msg);
+            sh.ps.println(msg);
         }
         // when signed in online player +1
         // when signed out offLine Players + 1 , online -1
